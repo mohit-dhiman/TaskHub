8dcf10e4ef0194b446c626cfcfb284faaa765e23
Mohit Dhiman <21983488+mohit-dhiman@users.noreply.github.com>
2025-10-24 10:22:21 +0530

    New Project

---
+++ src/test/java/md/java/taskhub/TaskHubApplicationTests.java
@@ -0,0 +1,13 @@
+package md.java.taskhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
